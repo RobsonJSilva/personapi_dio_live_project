06f4e815df15ba2e4b05c7e6a48b846ecef0dadc
Robson Silva <68351855+RobsonJSilva@users.noreply.github.com>
2021-08-02 09:18:33 -0300

    Developed person create operation

---
+++ src/test/java/one/digitalinnovation/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
